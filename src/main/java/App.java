7c5052921aa76167d60a6178adacfb71b751bfcf
awesomeo184 <awesomeo184@gmail.com>
2021-01-06 15:36:37 +0900

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+    }
+
+}
